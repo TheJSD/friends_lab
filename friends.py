c65fcc45202c00a4ffb13048d28aabd427ec186d
James Dickson <jamesstuartdickson@gmail.com>
2023-07-06 15:43:13 +0100

    Completed Task 4

--- friends.py
+++ friends.py
@@ -98,7 +98,11 @@ print(likes_to_eat(person3, "spinach"))
 # INPUT: person2, "Scrappy-Doo"
 # OUTPUT: None
 # Test your function by calling it and then printing our person2's list of friends
+def add_friend(person, new_friend):
+    person["friends"].append(new_friend)
 
+add_friend(person2, "Scrappy-Doo")
+print(person2["friends"])
 
 # 5. Define a function called remove_friend(person, old_friend) that removes a friend from the person's list of friends
 # INPUT: person2, "Fred"
